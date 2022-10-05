60cc161e624da575496809b8775d218cf2930f61
M. Evan Anindya W <m.evan.aw@gmail.com>
2022-10-05 11:02:40 +0700

    Add health books.

--- PeekABookWebApp/Pages/Index.cshtml.cs
+++ PeekABookWebApp/Pages/Index.cshtml.cs
@@ -11,6 +11,7 @@ namespace PeekABookWebApp.Pages
         public BooksRepositoryService BooksRepositoryService;
         public IEnumerable<Book> FantasyBooks { get; private set; }
         public IEnumerable<Book> SelfHelpBooks { get; private set; }
+        public IEnumerable<Book> HealthBooks { get; private set; }
 
         public IndexModel(ILogger<IndexModel> logger, BooksRepositoryService booksRepositoryService)
         {
@@ -18,13 +19,15 @@ namespace PeekABookWebApp.Pages
             BooksRepositoryService = booksRepositoryService;
             FantasyBooks = new List<Book>();
             SelfHelpBooks = new List<Book>();
+            HealthBooks = new List<Book>();
         }
 
         public async Task OnGetAsync()
         {
-            var fantasyTask = BooksRepositoryService.GetBooksBySubject("Fantasy", 3);
-            var selfHelpTask = BooksRepositoryService.GetBooksBySubject("\"Self+Help\"", 3);
-            var tasks = new List<Task> { fantasyTask, selfHelpTask };
+            var fantasyTask = BooksRepositoryService.GetBooksBySubject("Fantasy", 4);
+            var selfHelpTask = BooksRepositoryService.GetBooksBySubject("\"Self+Help\"", 4);
+            var healthTask = BooksRepositoryService.GetBooksBySubject("Health", 4);
+            var tasks = new List<Task> { fantasyTask, selfHelpTask, healthTask };
             while (tasks.Count > 0)
             {
                 Task finishedTask = await Task.WhenAny(tasks);
@@ -32,10 +35,14 @@ namespace PeekABookWebApp.Pages
                 {
                     if (fantasyTask.Result != null)
                         FantasyBooks = fantasyTask.Result;
-                } else
+                } else if (finishedTask == selfHelpTask)
                 {
                     if (selfHelpTask.Result != null)
                         SelfHelpBooks = selfHelpTask.Result;
+                } else
+                {
+                    if (healthTask.Result != null)
+                        HealthBooks = healthTask.Result;
                 }
                 tasks.Remove(finishedTask);
             }
