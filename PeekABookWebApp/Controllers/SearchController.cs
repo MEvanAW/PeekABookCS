a07ea2eeaa6e6c142a17febb0053372d7cce3bc3
M. Evan Anindya W <m.evan.aw@gmail.com>
2022-10-11 13:27:03 +0700

    implement search in search controller.

--- PeekABookWebApp/Controllers/SearchController.cs
+++ PeekABookWebApp/Controllers/SearchController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PeekABookWebApp.Services;
 using System.Text.Encodings.Web;
 
 namespace PeekABookWebApp.Controllers
@@ -6,9 +7,26 @@ namespace PeekABookWebApp.Controllers
     [Route("[controller]")]
     public class SearchController : Controller
     {
-        public string Index(string q)
+        public BooksRepositoryService BooksRepositoryService;
+        public SearchController(BooksRepositoryService booksRepositoryService)
         {
-            return HtmlEncoder.Default.Encode($"The result of {q}");
+            BooksRepositoryService = booksRepositoryService;
+        }
+
+        public async Task<string> Index(string q)
+        {
+            if (q == null || q == "")
+                return HtmlEncoder.Default.Encode("Empty result");
+            var task = BooksRepositoryService.GetBooks(q, 8);
+            var books = await task;
+            if (books.Count() == 0)
+                return HtmlEncoder.Default.Encode("Empty result");
+            string returnString = "";
+            foreach (var book in books)
+            {
+                returnString += HtmlEncoder.Default.Encode($"{book.Title}; ");
+            }
+            return returnString;
         }
     }
 }
